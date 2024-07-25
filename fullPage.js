5298e18570cb756913b0f56f136ad36e5dde7cac
Roman Chernetsky <misteryo.wins@gmail.com>
2024-07-25 16:51:52 +0300

    uploading the whole thing

---
+++ fullPage.js
@@ -0,0 +1,6 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor: ['#f6e0a4', '#ff608c', 'white', '#00c1b5', '#ff651a', '#ffbe00', '#1d3fbb', '#e30512']
+});
